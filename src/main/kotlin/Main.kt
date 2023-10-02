d29fb7a56cf0e051a3bf0860d21a6285bd8f764d
zearkiatos <caprilespe@outlook.com>
2023-10-02 19:26:38 -0300

    Variables in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
+const val PI = 3.1416
 fun main(args: Array<String>) {
     println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    var money:Int = 10
+    println(money)
+    money = 5
+    println(money)
+    val name = "Maria"
+    println(name)
+    println(PI)
 }
